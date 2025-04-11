e241b709bdfeab42f2eafa032ed0a55102add5de
anhqui04062004@gmail.com <qui0406>
2025-04-11 12:07:02 +0700

    them cong ty va hinh anh cong ty

--- backend_parttime_job/parttime_job_management/parttime_job/urls.py
+++ backend_parttime_job/parttime_job_management/parttime_job/urls.py
@@ -5,13 +5,16 @@ import oauth2_provider.views as oauth2_views
 from django.conf import settings
 from django_rest_passwordreset import urls as reset_password_urls
 from parttime_job.views import debug_token_view
+from rest_framework import routers
+
+
 
 router = DefaultRouter()
-# router.register('login', views.AuthUserLoginView, basename='login')
 router.register('user', views.UserViewSet, basename='register')
+router.register('company', views.CompanyViewSet, basename='company')
+router.register('company-list', views.CompanyListViewSet, basename='company-list')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('debug-token/', debug_token_view),  #
-    # path('send-email/', views.send_test_email, name='send_test_email')
+    path('debug-token/', debug_token_view), 
 ]
